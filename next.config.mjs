474ae34fac7130ada1aaead2232d70c9248ffec2
Eriberto Guzman <eriberto.guzman@outlook.com>
2024-01-28 01:22:29 -0500

    Add sass

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
-const path = require('path')
+import path from "path"
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   sassOptions: {
-    includePaths: [path.join(__dirname, "styles")],
+    includePaths: [path.join(path.dirname(import.meta.url), "styles")],
   }
 
 };
