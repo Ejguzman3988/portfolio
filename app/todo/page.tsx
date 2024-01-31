08f0f3c242c6acf0081a6ead67bfaa4b14530acb
Eriberto <eriberto.guzman@outlook.com>
2024-01-30 19:10:25 -0500

    add todo to db.json and pull using app router

--- app/todo/page.tsx
+++ app/todo/page.tsx
@@ -1,36 +1,35 @@
-import React from "react";
+interface todo {
+  id: number;
+  content: string;
+  completed: boolean;
+  order: null | number;
+  createdAt: Date;
+  updatedAt: Date;
+}
 
-const page = () => {
-  const todos = [
-    "make todos objects",
-    "create pseudo backend for todos - start w/ db.json",
-    "style todo page",
-    "Create navbar",
-    "Add transition to TODO page",
-    "Create ChangeLog",
-    "Add Styling for todo list",
-    "reverse this list",
-    "Research change log and follow style for this list",
-    "strike through todos",
-    "consider adding commits instead of actual todos",
-    "Create Zustand store to set up theming",
-    "Add theme light/dark",
-    "Create button for theme",
-    "Look into fonts",
-    "Consider color schemes",
-    "revisit homepage",
-  ];
+async function getTodos(): Promise<todo[]> {
+  const res = await fetch("http://localhost:8080/todos");
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch");
+  }
+
+  return res.json();
+}
+
+const Todo = async () => {
+  const todos = await getTodos();
 
   return (
     <div>
       <h1>Todo:</h1>
       <ul>
-        {todos.map((t, idx) => (
-          <li key={idx + t.slice(0, 2)}>{t}</li>
+        {todos.map((t) => (
+          <li key={t.id + t.content.slice(0, 2)}>{t.content}</li>
         ))}
       </ul>
     </div>
   );
 };
 
-export default page;
+export default Todo;
